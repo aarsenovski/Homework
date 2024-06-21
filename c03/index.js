504639fa8538b7bb502002a5ad5b0984f4167c11
Andrej <a.arsenovski@labdigital.nl>
2024-06-21 14:10:58 +0200

    add function with promise and async/await

--- c03/index.js
+++ c03/index.js
@@ -28,3 +28,25 @@ printDogName = (name) => {
 
 callbackFn(printCarName, "Mercedes");
 callbackFn(printDogName, "Lara");
+
+let areYouMilennial = (yourAge) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (yourAge >= 1981 && yourAge <= 1996) {
+        return resolve("You are a milenial");
+      } else return reject("You are not a milenial");
+    });
+  });
+};
+
+let milenialOrNot = async (yourAge) => {
+  try {
+    const approval = await areYouMilennial(yourAge);
+    console.log(approval);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+milenialOrNot(1974);
+milenialOrNot(1994);
