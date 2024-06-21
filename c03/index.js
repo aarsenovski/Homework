8bb8a6901eef6b9d1a2e7b421a4be37187440ea7
Andrej <a.arsenovski@labdigital.nl>
2024-06-21 14:05:11 +0200

    add callbacks fns

--- c03/index.js
+++ c03/index.js
@@ -5,8 +5,26 @@ const { writeFile, appendFile, readFile } = require("./test-andrej2.js");
 printMyName("Andrej");
 showCurrentTime();
 
+// Write, append, read functions
 writeFile("data.txt", "ova e random tekst22");
-appendFile("data.txt", `\n\some additional text from the append`);
+appendFile(
+  "data.txt",
+  `\n\some additional text from the append \n\Ime: Andrej \n\Godini: 29 \n\Grad: Skopje`
+);
 readFile("data.txt")
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
+
+// Callbacks
+callbackFn = (fn, name) => {
+  fn(name);
+};
+printCarName = (name) => {
+  console.log(name);
+};
+printDogName = (name) => {
+  console.log(name);
+};
+
+callbackFn(printCarName, "Mercedes");
+callbackFn(printDogName, "Lara");
