a0329a11f6f29f6974ccae9962a650287c415e17
Andrej <a.arsenovski@labdigital.nl>
2024-07-03 16:29:22 +0200

    rewrite using Express

--- c06/handler/handler.js
+++ c06/handler/handler.js
@@ -1,21 +1,14 @@
-handleUser = (req, res, username) => {
+const handleUser = (req, res) => {
   const usernamePattern2 = /^[A-Z]+\.[0-9]{4}$/; //ANDREJ.1994
+  const { username } = req.query;
 
   if (usernamePattern2.test(username)) {
-    res.end(`Hello ${username}`);
+    res.send(`Hello ${username}`);
   } else {
-    res.end("Invalid username");
+    res.send("Invalid username");
   }
 };
 
 module.exports = {
   handleUser,
-};
-
-const patterns = {
-  usernamePattern: /^[\w]{3,10}$/, //andrej_01
-  emailPattern: /^([a-zA-Z\d]+)@([a-zA-Z\d]{3,10}).([a-z]{2,5})$/, // andrejarsenovski@gmail.com
-  addressPattern: /^[a-zA-Z]{5,15}\s?[a-zA-Z]+? [\w]{5,15},[\d]{4}$/, //Partizanska Skopje,1000 //Markova Reka, Skopje,1050
-  usernamePattern3: /^[a-z]{4,10}/,
-  cityAndPostalCodePattern: /^[A-Z]{1}[a-z]+-[\d]{4}$/, //Skopje-1000
 };
--- c06/index.js
+++ c06/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const { handleUser } = require("./handler/handler");
 
+const app = express();
+app.get("/user", handleUser);
 
-  if (parsedUrl.pathname == "/user") {
-    handleUser(req, res, parsedUrl.query.username);
-  }
-});
-server.listen(3000, () => {
-  console.log("Server started at port 3000");
+app.listen(3000, () => {
+  console.log(`Server started at port 3000`);
 });
